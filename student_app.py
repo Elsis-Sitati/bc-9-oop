fad10ed6f96bf557c605eb01e6911bbedcfdbf45
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-19 00:14:51 +0300

    [student_app.py] Make Student objects
    Insatiating the student class with 10 objects

---
+++ student_app.py
@@ -0,0 +1,26 @@
+
+from student import Student
+
+# Create at least 10 students
+
+s1 = Student('Kevin', 'Chiteri')
+s2 = Student('Allan', 'M.', 'UG')
+s3 = Student('Elsis', 'Sitati')
+s4 = Student('Jane', 'Kugo')
+s5 = Student('Beth', 'Wanjiku')
+s6 = Student('Rehema', 'Wachira')
+s7 = Student('Innocent', 'Kateba')
+s8 = Student('Mike', 'Dean')
+s9 = Student('Steve', 'Njoro')
+s10 = Student('Lucious', 'Lyon')
+
+# Make at least 5 students attend class
+
+# e.g
+s1.attend_class(...)
+
+
+
+# You should be able to print the list of
+# students who attend class on particular day
+
