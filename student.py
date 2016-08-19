6432b99265720c7149be762a005eb90adc27e2bf
Elsis-Sitati <sitatielsis@gmail.com>
2016-08-19 17:23:26 +0300

    [student.py] Modify attend_class method
    Added a dictionary to store kwargs arguments

--- student.py
+++ student.py
@@ -1,61 +1,56 @@
-from datetime import date
-
+import datetime
 
 map_ = {
+    'UG': 'Uganda',
     'KE': 'Kenya',
     'NG': 'Nigeria',
-    'UG': 'Uganda',
     'TZ': 'Tanzania'
 }
 
+class Student(object):
+    #declaring a variable Id with a value zero which is an instance  variable
+    Id = 0  
+    #Create an empty dictionary attendace that stores the attendance of the students                                                    
+    attendance = {}                                            
 
-class Student(object): #inheritance
-
-	# class properties
-    count = 0
-    attended_today = []
-    
     def __init__(self, fname, lname, cc='KE'):
-        # generate sequential unique ID
-        Student.count += 1
-        self.__id = Student.count # fake private
+        #Increment the Student Id everytime this class is called so that everyone has a unique Id
+        Student.Id = Student.Id + 1                            
+
+        self.unique_id = Student.Id
         self.fname = fname
         self.lname = lname
         self.country = map_[cc]
 
-	def attend_class(self, **kwargs):
-		'''
-           	default values:
-            location  = 'Hogwarts'
-            date      = current_date
-            teacher   = 'Alex'
+
+    def attend_class(self, **kwargs):
         '''
-        # declare variables to hold whataver value is gotten at a certain key
-        # if no value is gotten the default value is asssigned to the variable
-        location = kwargs.get('location', 'Hogwarts')
-        what_day = kwargs.get('date', date.today())
-        teacher  = kwargs.get('teacher', 'Alex')
-        sentence  = self.fname + ' attended class taught by ' + teacher + ' at ' + location + ' on ' + str(what_day)
-        # first make full_name to be an empty string
-        full_name = ''
-        # condition for it the day in arguments is the same as todays day
-        # if they match then append the full names to the empty attendee_today list declared above
-        if str(what_day) == str(date.today()):
-            full_name = self.fname + ' ' + self.lname
-            Student.todays_attended_today.append(full_name)
-        return sentence
+        defaualt values:
+            loc = 'Hogwarts'
+            date = 'today'
+            teacher = 'Alex'
+        '''
+
+        loc = kwargs.get('loc', 'Hogwarts')
+        date = kwargs.get('date', datetime.date.today())
+        teacher = kwargs.get('teacher', 'Alex')
+
+        if date not in Student.attendance.keys(): 
+         #Check if each date is in the attendance list, if not, add both the current date and the full name of the student                                 
+            Student.attendance[date] = [self.fname + " " + self.lname]
+        else:
+            full_name = self.fname +  " " + self.lname
+            if full_name not in Student.attendance[date]:
+                Student.attendance[date].append(full_name)
 
     @staticmethod
-    def get_attendance_list():
-        '''
-            Method should generate a list of people who attended class in that day
-        '''
-        # declarea variable came_today that stores people who came today from attended_today list
-        came_today = Student.attended_today
-        sentence = "\n".join(came_today)
-        return sentence
-    
-    
-    
+    def attendance_list(date):
+        if date not in Student.attendance.keys(): 
+          #Print the records for the current date, if none, then no  attendance                                
+            print("No Attendance today!")
+        else:
+            the_attendance_list = Student.attendance[date]
 
-
+            print("Attendance on: " + str(date))
+            for student in the_attendance_list:
+                print(student)
